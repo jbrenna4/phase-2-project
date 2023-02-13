e64f7a219b8aba4e6f73b44776ab25be80f336a2
Jim Brennan <jim.j.brennan@gmail.com>
2023-02-13 18:30:17 -0500

    header, navbar, search, filter, bio, contact are built but not wired up

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,19 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
+    <header className="header">
       <h1>
-        Plantsy
+        Bob Ross
         <span className="logo" role="img">
-          🌱
+        🖌️
         </span>
       </h1>
+      <a href="https://www.instagram.com/bobross_thejoyofpainting/?hl=en">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png" />
+      </a>
+      <a href="https://twitter.com/BobRossOfficial?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+        <img src="https://cdn-icons-png.flaticon.com/512/124/124021.png" />
+      </a>      
     </header>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,43 @@
 import React from "react";
 
+
 function NavBar() {
     return (
-      <header>
-        <h1>
-          Plantsy
-          <span className="logo" role="img">
-            🌱
-          </span>
-        </h1>
-      </header>
-    );
-  }
+    <nav class="topnav">
+        <ul>
+          <li>
+            <a href="/HomePage">Home</a>
+          </li>
+          <li>
+            <a href="./Bio">Bio</a>
+          </li>
+          <li>
+            <a href="./PaintingList">Paintings</a>
+          </li>
+          <li>
+            <a href="./contact">Contact</a>
+          </li>
+          <div class="search">
+            <select id="select-color" placeholder="Pick a color...">
+                <option value="">Pick a color...</option>
+                <option value="red">Red</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="brown">Brown</option>
+                <option value="yellow">Yellow</option>
+                <option value="black">Black</option>
+                <option value="white">White</option>
+              </select>
+          </div>
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" />
+              Sort by season
+            </label>
+          </div>
+        </ul>
+    </nav>
+  )
+}
   
   export default NavBar;
