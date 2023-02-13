03a091762b24f2406b0d6bdaf9e0b9e35bf889fc
Jim Brennan <jim.j.brennan@gmail.com>
2023-02-13 11:36:05 -0500

    initial commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+function HomePage({searchTerm, onChange}) {
+
+  function handleChange(e){
+    onChange(e.target.value);
+
+  }
+
+
+  return (
+    <div className="searchbar">
+      <label htmlFor="search">Search Plants:</label>
+      <input
+        type="text"
+        id="search"
+        placeholder="Type a name to search..."
+        onChange={handleChange}
+        value = {searchTerm}
+      />
+    </div>
+  );
+}
+
+export default HomePage;
