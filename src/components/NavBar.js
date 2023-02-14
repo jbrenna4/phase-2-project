6ad67f1b9e0d4031054aaad275f3d5f03e258536
Ash <ashjames.dean@gmail.com>
2023-02-14 14:52:36 -0500

    start of the random Quote button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import RandomQuote from "./RandomQuote";
 
 
 function NavBar() {
@@ -35,6 +36,7 @@ function NavBar() {
               Sort by season
             </label>
           </div>
+          <RandomQuote />
         </ul>
     </nav>
   )
