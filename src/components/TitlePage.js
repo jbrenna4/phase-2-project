d049d2384e6c0006468a4c03e32b4d9eaea9a3e6
Ash <ashjames.dean@gmail.com>
2023-02-17 11:19:59 -0500

    styling on landingpage

--- src/components/TitlePage.js
+++ src/components/TitlePage.js
@@ -4,11 +4,32 @@ function TitlePage() {
 
     return (
         <div className="title">
-            <h2> Bob Ross </h2>
-            <img 
+            <h2 className="titleText"> "Let's get crazy" </h2>
+            <img class="titleImage" 
                 src="https://static01.nyt.com/images/2019/07/13/arts/13video/bob-ross-cover-superJumbo-v2.png" 
                 alt="Bob Ross"
             />
+            <img class="titleImage" 
+                src="https://static01.nyt.com/images/2019/07/13/arts/13video/bob-ross-cover-superJumbo-v2.png" 
+                alt="Bob Ross"
+            />
+            <img class="titleImage" 
+                src="https://static01.nyt.com/images/2019/07/13/arts/13video/bob-ross-cover-superJumbo-v2.png" 
+                alt="Bob Ross"
+            />
+            <img class="titleImage" 
+                src="https://static01.nyt.com/images/2019/07/13/arts/13video/bob-ross-cover-superJumbo-v2.png" 
+                alt="Bob Ross"
+            />
+            <img class="titleImage" 
+                src="https://static01.nyt.com/images/2019/07/13/arts/13video/bob-ross-cover-superJumbo-v2.png" 
+                alt="Bob Ross"
+            />
+            <img class="titleImage" 
+                src="https://static01.nyt.com/images/2019/07/13/arts/13video/bob-ross-cover-superJumbo-v2.png" 
+                alt="Bob Ross"
+            />
+            
         </div>
 
 
