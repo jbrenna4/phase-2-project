785bb94e2d761c183f26937f9b58bab079aa65c9
Jim Brennan <jim.j.brennan@gmail.com>
2023-02-17 10:25:40 -0500

    cleaned up some comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,10 @@
-//import './App.css';
 import React from "react";
 import Header from "./Header";
 import HomePage from "./HomePage";
-import NavBar from "./NavBar";
 
-//import NavBar from "./NavBar";
-
-//import RandomQuote from "./RandomQuote";
 
 function App() {
 
- 
-//  const [searchTerm, setSearchTerm] = useState("");
-//   console.log(searchTerm);
-
-  //const filteredPaintings = paintings.filter((painting) => console.log(painting));
 
   return (
     <div className="app">
